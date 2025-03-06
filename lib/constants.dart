2760e556d2f64acbb99c9c81e95694cdc59d08bb
Stefania <picioreastefania@yahoo.com>
2025-03-06 13:42:14 -0500

    Initial Commit
    Pushed the skeleton of the app

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+#This file will contain the background/font colors, text styles and other design components that we will use throughout the app
+#It is useful to only use constants because if we wanted to change the style of the headers for example, ity will only have to be done once here and it will apply to the entire app.
