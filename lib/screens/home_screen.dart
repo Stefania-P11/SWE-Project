f76ada4a5b4ffb80100bc115636bb3f7ed6c1a36
Stefania <picioreastefania@yahoo.com>
2025-03-24 21:34:44 -0400

    Connected the "Play it Safe" and "Surprise Me" buttons to take the user to the Outfit Suggestion Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:dressify_app/constants.dart';
+import 'package:dressify_app/screens/display_outfit_screen.dart';
 import 'package:dressify_app/widgets/custom_app_bar.dart';
 import 'package:dressify_app/widgets/custom_bottom_navbar.dart';
 import 'package:dressify_app/widgets/custom_button_2.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,10 +12,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-    
+
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 239, 240, 240),
-      
+
       //App Bar
       appBar: CustomAppBar(),
 
@@ -42,9 +42,7 @@ class HomeScreen extends StatelessWidget {
                 //Temperature
                 Text(
                   '54°F', // TODO: Pull actual weather data
-                  style: GoogleFonts.lato(
-                    textStyle: kBodyLarge
-                ),
+                  style: GoogleFonts.lato(textStyle: kBodyLarge),
                 ),
                 Text(
                   '37° - 64°', // TODO: Pull actual weather data. I am also not sure what the range represents--maybe we don't need this
@@ -52,18 +50,13 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
-            
-            
+
             // Insights
             Column(
               spacing: screenHeight * 0.012,
               children: [
-                Text(
-                  'Insight your wardrobe',
-                  textAlign: TextAlign.center,
-                  style: kH3
-  
-                ),
+                Text('Insight your wardrobe',
+                    textAlign: TextAlign.center, style: kH3),
                 Container(
                   padding: EdgeInsets.all(screenWidth * 0.05),
                   child: Row(
@@ -81,9 +74,7 @@ class HomeScreen extends StatelessWidget {
                           Text(
                             'Tops',
                             textAlign: TextAlign.center,
-                            style: GoogleFonts.playfairDisplay(
-                              textStyle: kH3
-                          ),
+                            style: GoogleFonts.playfairDisplay(textStyle: kH3),
                           )
                         ],
                       ),
@@ -137,34 +128,52 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
 
-
             // Buttons
             Container(
               // padding: EdgeInsets.all(20),
               child: Column(
                 spacing: screenHeight * 0.01,
                 children: [
-                    CustomButton2(
-                  text: 'CREATE OUTFIT',
-                  onPressed: () {
-                      // TODO: Implement button functionality
-                  },
-                  ),
-
                   CustomButton2(
-                  text: 'PLAY IT SAFE',
-                  onPressed: () {
+                    text: 'CREATE OUTFIT',
+                    onPressed: () {
                       // TODO: Implement button functionality
-                  },
+                    },
                   ),
-
-                 CustomButton2(
-                  text: 'SURPRISE ME',
-                  onPressed: () {
-                      // TODO: Implement button functionality
-                  },
+                  CustomButton2(
+                    text: 'PLAY IT SAFE',
+                    onPressed: () {
+                      // TODO: befire navigating we need to select an outfit from favorites, retrieve each item's ID
+                      // and pass them to the OutfitSuggestionScreen so the images can be displayed
+                      // The OutfitSuggestionScreen will be modidfied to maybe take 3 parameters: topId, bottomID and shoeID
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const OutfitSuggestionScreen(),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ),
+                      );
+                    },
                   ),
-
+                  CustomButton2(
+                    // TODO: Implement AI feature and retrieve the ID's of the outfit components to pass to OutfitSuggestionScreen.
+                    text: 'SURPRISE ME',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const OutfitSuggestionScreen(),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ),
+                      );
+                    },
+                  ),
                 ],
               ),
             ),
