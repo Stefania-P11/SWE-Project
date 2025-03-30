86c070a71ac1c03ce9000194ba21320d0c58b1cf
Stefania <picioreastefania@yahoo.com>
2025-03-30 10:50:53 -0400

    Replaced username with the correct constant. Updated the countItemsPerCategory to reset counter every time it is called to avoid repeated incrementing every time.

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,46 +1,91 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-class Item{
-  late final String category;
-  late final String color;
-  late final int id;
-  late final String label;
-  int timesWorn;
-  late final String url;
-  late final String weather;
+import 'package:dressify_app/constants.dart';
+
+// Item class to model clothing items
+class Item {
+  // Properties to store item details
+  late final String category;   // Category of the item (e.g., Top, Bottom, Shoes)
+  late final String color;      // Color of the item
+  late final int id;            // Unique ID for the item
+  late final String label;      // Label or name of the item
+  int timesWorn;                // Number of times the item has been worn
+  late final String url;        // URL for the item's image
+  late final String weather;    // Weather conditions suited for the item
+
+  // Static list to store fetched items
   static List<Item> itemList = [];
-  static int topCount = 0;
-  static int bottomCount = 0;
-  static int shoeCount = 0;
-  Item({required this.category, required this.color, required this.id, required this.label, required this.timesWorn, required this.url, required this.weather});
 
+  // Static counters to store item counts by category
+  static int topCount = 0;      // Count of tops
+  static int bottomCount = 0;   // Count of bottoms
+  static int shoeCount = 0;     // Count of shoes
+
+  // Constructor to initialize an Item
+  Item({
+    required this.category,
+    required this.color,
+    required this.id,
+    required this.label,
+    required this.timesWorn,
+    required this.url,
+    required this.weather,
+  });
+
+  // Factory constructor to create an Item from Firestore data
   factory Item.fromFirestore(DocumentSnapshot doc) {
+    // Map Firestore document data to a Map
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    // Create and return an Item with data from Firestore
     return Item(
+      category: data['category'],   // Assign category from Firestore
+      color: data['color'],         // Assign color from Firestore
+      id: data['id'],               // Assign item ID from Firestore
+      label: data['label'],         // Assign label from Firestore
+      timesWorn: data['timesWorn'], // Assign times worn from Firestore
+      url: data['url'],             // Assign URL from Firestore
 
-      category : data['category'],
-      color : data['color'],
-      id : data['id'],
-      label : data['label'],
-      timesWorn : data['timesWorn'],
-      url : data['url'],
-      weather : data['weather'],
-
+      // Handle weather field which can be either a string or a list
+      weather: data['weather'] is String
+          ? data['weather'] // If it's a String, assign directly
+          : (data['weather'] is List<dynamic> 
+              ? (data['weather'] as List<dynamic>).join(', ') // Join list items into a string
+              : ''), // If neither, assign an empty string
+              // NOTE: we might need to add some checks here  because all attributes are required
+              // so weather cannot be the empty string. Using it for now to avoid errors.
     );
   }
-  static Future<void> fetchItems(String username) async{
-    FirebaseFirestore db = FirebaseFirestore.instance;  
-    QuerySnapshot querySnapshot = await db.collection('users').doc(username).collection("Clothes").get();
+
+  // Fetch items from Firestore and populate itemList
+  static Future<void> fetchItems(String username) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+
+    // Query to get items from the "Clothes" collection of the user
+    QuerySnapshot querySnapshot = await db
+        .collection('users')
+        .doc(kUsername)
+        .collection("Clothes")
+        .get();
+
+    // Map each document to an Item and populate itemList
     itemList = querySnapshot.docs.map((doc) => Item.fromFirestore(doc)).toList();
   }
-  static Future<void> countItemsPerCategory() async{
-    for(Item x in itemList){
-      if(x.category == 'Top'){
-        Item.topCount++;
-      }else if(x.category == 'Bottom'){
-        Item.bottomCount++;
-      }else if(x.category == 'Shoes'){
-        Item.shoeCount++;
+
+  // Count items per category (Top, Bottom, Shoes)
+  static Future<void> countItemsPerCategory() async {
+    // Reset counters to avoid double counting
+    Item.topCount = 0;
+    Item.bottomCount = 0;
+    Item.shoeCount = 0;
+
+    // Iterate through itemList and count items by category
+    for (Item x in itemList) {
+      if (x.category == 'Top') {
+        Item.topCount++; // Increment top counter if category is 'Top'
+      } else if (x.category == 'Bottom') {
+        Item.bottomCount++; // Increment bottom counter if category is 'Bottom'
+      } else if (x.category == 'Shoes') {
+        Item.shoeCount++; // Increment shoe counter if category is 'Shoes'
       }
     }
   }
