948182884b34e3beaa72ddb207332f061693713f
matmoja <mateenmoj@gmail.com>
2025-04-01 00:14:42 -0400

    Backend functions

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -46,23 +46,47 @@ class FirebaseService{
 
   //add outfit to Firestore
   static addFirestoreOutfit(String category, int id, Item top, Item bottom, Item shoes, int timesWorn, List<String> weather){
+    final outfitStorage = {
+      'category' : category,
+      'id' : id,
+      'top' : top.id,
+      'bottom' : bottom.id,
+      'shoes' : shoes.id,
+      'number of times worn' : timesWorn,
+      'weather' : weather,
+    };
+
+    db.collection('users').doc(kUsername).collection('Outfits').doc(id.toString()).set(outfitStorage);
     return 0;
   }
 
   //add outfit locally
   static addLocalOutfit(String category, int id, Item top, Item bottom, Item shoes, int timesWorn, List<String> weather){
     //Calls outfit constructor and then add it to Outfit.outfitList
+    Outfit outfitSet = Outfit(
+      label : category,
+      id : id,
+      topItem : top,
+      bottomItem : bottom,
+      shoeItem : shoes,
+      timesWorn : timesWorn,
+      weather : weather,
+    );
+
+    Outfit.outfitList.add(outfitSet);
     return 0;
   }
 
   //remove outfit from Firestore
   static removeFirestoreOutfit(Outfit outfit){
+    db.collection('users').doc(kUsername).collection('Outfits').doc(outfit.toString()).delete();
     return 0;
   }
 
   //remove local outfit from Outfit.outfitList. Outfit arg should already be a part of outfitList, so this is trivial
   static removeLocalOutfit(Outfit outfit){
     //remove outfit from Outfit.outfitList if outfit.id matches list element's id
+    Outfit.outfitList.removeWhere((o) => o.id == outfit.id);
     return 0;
   }
 }
