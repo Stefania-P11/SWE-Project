f8480ee4cd78a14e7aa893691fb1326f39e2d2b4
Stefania <picioreastefania@yahoo.com>
2025-04-21 23:18:29 -0400

    Moved Play It Safe feature in the services directory and deleted the features directory

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // Constant for the username used in Firestore
-const String kUsername = 'dummy';
+const String kUsername = 'stefania';
 
 // COLORS
 
